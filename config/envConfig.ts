b34e41919f8c68f318a9e807585deaa77566cc2d
DbHridoy <mdnuralam378@gmail.com>
2026-07-20 05:28:28 +0600

    fix(branch-manager): add branch manager from global admin

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -1,5 +1,5 @@
 const backendUrl =
-  process.env.NEXT_PUBLIC_BACKEND_URL?.replace(/\/+$/, "") || "https://fisheries-half-nodes-concepts.trycloudflare.com";
+  process.env.NEXT_PUBLIC_BACKEND_URL?.replace(/\/+$/, "") || "http://localhost:5004";
 export const imgUrl = backendUrl;
 export const url = `${backendUrl}/api/v1/`;
 export const getBaseUrl = () => url;
